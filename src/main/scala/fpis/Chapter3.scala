5ac46c07464dd14cefdb040022de9773893d6e5c
sungkmi <sungkmi@gmail.com>
2015-06-22 21:45:56 +0900

    exercise 3.3

--- src/main/scala/fpis/Chapter3.scala
+++ src/main/scala/fpis/Chapter3.scala
@@ -3,9 +3,14 @@ package fpis
 import fpinscala.datastructures._
 
 object Chapter3 extends App {
-  
+
   def tail[A](as: List[A]): List[A] = as match {
     case Cons(head, tail) => tail
-    case Nil => Nil    
+    case Nil              => Nil
+  }
+
+  def setHead[A](as: List[A], head: A): List[A] = as match {
+    case Cons(_, tail) => Cons(head, tail)
+    case Nil           => Nil
   }
 }
--- src/test/scala/fpis/Chapter3Test.scala
+++ src/test/scala/fpis/Chapter3Test.scala
@@ -8,6 +8,11 @@ import Chapter3._
 class Chapter3Test extends FunSuite {
 
   test("Exercise 3.2") {
-    assert(tail(List(1, 2, 3, 4)) === List(2,3,4))
+    assert(tail(List(1, 2, 3, 4)) === List(2, 3, 4))
+  }
+
+  test("Exercise 3.3") {
+    assert(setHead(List(1, 2, 3, 4), 10) === List(10, 2, 3, 4))
+    assert(setHead(Nil, 10) === Nil)
   }
 }
