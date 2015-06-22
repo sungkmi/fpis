b2f87c23a0b79ab99078fcb67875f2f143aa4908
sungkmi <sungkmi@gmail.com>
2015-06-22 20:33:27 +0900

    exercise 2.2

--- src/main/scala/fpis/Chapter2.scala
+++ src/main/scala/fpis/Chapter2.scala
@@ -8,7 +8,18 @@ object Chapter2 extends App {
       else {
         fibTuple(i + 1, (last._2, last._1 + last._2))
       }
-    
+
     fibTuple(1, (0, 1))._1
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n == as.size - 1) true
+      else if (ordered(as(n), as(n + 1))) loop(n + 1)
+      else false
+    }
+
+    if (as.size < 2) true else loop(0)
   }
 }
--- src/test/scala/fpis/Chapter2Test.scala
+++ src/test/scala/fpis/Chapter2Test.scala
@@ -12,4 +12,10 @@ class Chapter2Test extends FunSuite {
     assert(fib(3) === 1)
     assert((1 to 6 map fib) === Seq(0, 1, 1, 2, 3, 5))
   }
+
+  test("Exercise 2.2") {
+    assert(isSorted[Int](Array(1, 2, 3), _ < _) === true)
+    assert(isSorted[Int](Array(3, 2, 1), _ < _) === false)
+    assert(isSorted[Int](Array.empty[Int], _ < _) === true)
+  }
 }
